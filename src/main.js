a32c24f702754a3c7fbeaab4344274c9c567bffe
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-29 23:26:29 +0630

    Using vuex modules

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import './main.scss'
+import store from './store'
 
-createApp(App).mount('#app')
+
+createApp(App)
+    .use(store)
+    .mount('#app')
